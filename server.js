048721e71da4b8239f425ce809512d162c3a8cbd
thomastam73 <thomastam73@gmail.com>
2021-04-21 16:43:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require("./app");
 // Server
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
+  app.use(bodyParser.json({ limit: "20mb" }));
   // eslint-disable-next-line no-console
   console.log(`Deaf and Dumb People API Server is now running on port ${PORT}`);
 });
